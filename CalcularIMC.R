14be127de05ce405978cc40a19bb2bd5ab2ce5cf
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-02-25 19:03:23 -0300

    Create CalcularIMC.R

---
+++ CalcularIMC.R
@@ -0,0 +1,6 @@
+altura <- as.numeric(readline("Digite sua altura (m):"))
+peso <- as.numeric(readline("Digite seu peso (kg):"))
+imc <- peso / (altura^2)
+print(paste("seu imc é:",imc))
+x - 2
+x * 4
