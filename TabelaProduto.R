190ae8e3a7b6bef5e19e70b0d0d238db44337428
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-02-25 20:36:36 -0300

    Create TabelaProduto.R

---
+++ TabelaProduto.R
@@ -0,0 +1,19 @@
+# vetor
+notas <- c(7.5, 8.0, 9.2)
+print(mean(notas))
+# data frame
+vendas <- data.frame(
+  produto = c("A", "B" , "C"),
+  quantidade = c(10, 15, 5)
+)
+print(vendas)
+
+
+
+
+produto <- list(
+  nome = "Notebook",
+  preco = 2500,
+  estoque = 10
+)
+print(nome(produto))
